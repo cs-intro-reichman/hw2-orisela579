53014f95e971a9e8e68fca05a5ab833753acfec0
Ori Sela <orisela579@gmail.com>
2025-11-08 16:52:10 +0200

    first try

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,26 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String name = args[0];
+            int n = Integer.parseInt(args[1]);
+            char letter;
+            for(int i = 0; i<name.length(); i++)
+            {
+                String x =" a";
+                letter = name.charAt(i);
+                if (letter=='A'|| letter=='E' || letter=='F' || letter=='H' || letter=='I' || letter=='L' || letter=='M' || letter=='N' || letter=='O' || letter=='R' || letter=='S' || letter=='X') {
+                      x = "an";  
+                }
+                System.out.println("Give me " + x + " " + letter + ": " + letter + "!");
+
+            }
+
+
+
+            System.out.println("What does that spell?");
+            for(int i = 0; i < n; i++)
+            {
+                System.out.println(name + "!!!");
+            }
         }
 }
